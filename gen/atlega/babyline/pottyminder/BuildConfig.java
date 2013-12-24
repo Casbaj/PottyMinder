1cecd06049018791722c37bea00ba90c44974301
Casbaj <kmatshaba@multichoice.co.za>
2013-12-24 15:05:10 +0200

    Initial Upload

---
+++ gen/atlega/babyline/pottyminder/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package atlega.babyline.pottyminder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/atlega/babyline/pottyminder/Timer.java
@@ -0,0 +1,17 @@
+package atlega.babyline.pottyminder;
+
+public class Timer {
+	long startTime;
+	long elapsed;
+	int seconds;
+	int minutes;
+	int hours;
+	String displayText;
+	
+	public Timer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
