cd028d5243a2f9c9fe7b4a934cf7c289437a0c4e
Casbaj <kmatshaba@multichoice.co.za>
2014-01-04 17:52:47 +0200

    Updates...Added BubbleTextView for a timer control

--- src/atlega/babyline/pottyminder/MainActivity.java
+++ src/atlega/babyline/pottyminder/MainActivity.java
@@ -16,57 +16,63 @@ public class MainActivity extends Activity {
 	private long timeInMillies = 0l;
 	private long timeSwap = 0l;
 	private TextView txtTimer;
+	private BubbleTextView bubbleTimer;
 	private int direction;
 	private final int DIRECTION_UP = 0;
 	private final int DIRECTION_DOWN = 1;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        txtTimer = (TextView) findViewById(R.id.txtTimer);
-        direction = DIRECTION_DOWN;
-    }
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public void onStart(View view){
-    	startTime = SystemClock.uptimeMillis();
-    	myHandler.postDelayed(updateTimerMethod, 0);
-    }
-    
-    public void onStop(View view){
-    	timeSwap += timeInMillies;
-    	myHandler.removeCallbacks(updateTimerMethod);
-    }
-    
-    private Runnable updateTimerMethod = new Runnable() {
-    	public void run() {
+		txtTimer = (TextView) findViewById(R.id.txtTimer);
+		bubbleTimer = (BubbleTextView) findViewById(R.id.bubbleTextView1);
+		direction = DIRECTION_DOWN;
+	}
 
-    	if (direction == DIRECTION_UP){
-    		timeInMillies = SystemClock.uptimeMillis() - startTime;
-    		finalTime = timeSwap + timeInMillies;
-    	} else if (direction == DIRECTION_DOWN) {
-    		timeInMillies = SystemClock.uptimeMillis() - startTime + 25000;
-    		finalTime = timeSwap + timeInMillies;
-    	}
-    	
-    	int seconds = (int) (finalTime / 1000);
-    	int minutes = seconds / 60;
-    	seconds = seconds % 60;
-    	int milliseconds = (int) (finalTime % 1000);
-    	txtTimer.setText(""+ minutes + ":"
-    	+ String.format("%02d", seconds) + ":"
-    	+ String.format("%03d", milliseconds));
-    	myHandler.postDelayed(this, 0);
-    	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    	};
+	public void onStart(View view) {
+		startTime = SystemClock.uptimeMillis();
+		myHandler.postDelayed(updateTimerMethod, 0);
+	}
+
+	public void onStop(View view) {
+		timeSwap += timeInMillies;
+		myHandler.removeCallbacks(updateTimerMethod);
+	}
+
+	private Runnable updateTimerMethod = new Runnable() {
+		public void run() {
+
+			if (direction == DIRECTION_UP) {
+				timeInMillies = SystemClock.uptimeMillis() - startTime;
+				finalTime = timeSwap + timeInMillies;
+			} else if (direction == DIRECTION_DOWN) {
+				timeInMillies = SystemClock.uptimeMillis() - startTime + 25000;
+				finalTime = timeSwap + timeInMillies;
+			}
+
+			int seconds = (int) (finalTime / 1000);
+			int minutes = seconds / 60;
+			seconds = seconds % 60;
+			int milliseconds = (int) (finalTime % 1000);
+			txtTimer.setText("" + minutes + ":"
+					+ String.format("%02d", seconds) + ":"
+					+ String.format("%03d", milliseconds));
+			
+			bubbleTimer.setText("" + minutes + ":"
+					+ String.format("%02d", seconds) + ":"
+					+ String.format("%03d", milliseconds));
+			
+			myHandler.postDelayed(this, 0);
+		}
+
+	};
 }
